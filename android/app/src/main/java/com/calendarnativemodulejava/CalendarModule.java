7896b7134f392884c5dd069da2886643201580d7
Ezequiel De Oliveira <ezequiel1de1oliveira@gmail.com>
2022-03-13 14:52:58 -0300

    Open intent throug java module

--- android/app/src/main/java/com/calendarnativemodulejava/CalendarModule.java
+++ android/app/src/main/java/com/calendarnativemodulejava/CalendarModule.java
@@ -1,5 +1,7 @@
 package com.calendarnativemodulejava;
 
+import android.content.Intent;
+import android.provider.CalendarContract;
 import android.util.Log;
 
 import com.facebook.react.bridge.ReactApplicationContext;
@@ -7,7 +9,7 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 
 public class CalendarModule extends ReactContextBaseJavaModule {
-
+    ReactApplicationContext context = getReactApplicationContext();
     CalendarModule(ReactApplicationContext context) {
         super(context);
     }
@@ -21,5 +23,14 @@ public class CalendarModule extends ReactContextBaseJavaModule {
     public void createCalendarEvent(String name, String location) {
         Log.d("CalendarModule", "Create event called with name: " + name
                 + " and location: " + location);
+        Intent intent = new Intent(Intent.ACTION_EDIT)
+            .setData(CalendarContract.Events.CONTENT_URI)
+            .putExtra(CalendarContract.Events.TITLE, name)
+            .putExtra(CalendarContract.Events.EVENT_LOCATION, location)
+            .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, System.currentTimeMillis())
+            .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, System.currentTimeMillis() + (60 * 60 * 1000))
+            .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        context.startActivity(intent);
     }
 }
